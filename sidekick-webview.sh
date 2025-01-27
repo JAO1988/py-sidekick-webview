6e2883c050a8c3c4bca9ff11132c554d39cb2570
JAO1988 <92758759+JAO1988@users.noreply.github.com>
2025-01-27 07:20:19 -0500

    Update sidekick-webview.sh

--- sidekick-webview.sh
+++ sidekick-webview.sh
@@ -8,6 +8,6 @@ script_dir="$(dirname "$(readlink -f "$0")")"
 item64=$(echo -n "$new_clip" | base64) ## Encode and paste copied base64 item in Sidekick Market URL
 url="http://localhost:5000/trade/xurl_${item64}"
 sleep 0.1
-python ".dir/poe.py" "$url" ## Point to the directory where Sidekick Webview script is located
+python ".dir/sidekick-webview.py" "$url" ## Point to the directory where Sidekick Webview script is located
 
 echo -n "$item" | wl-copy ## Restore original item name in text-string format
